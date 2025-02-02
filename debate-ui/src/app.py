cc7c03028af95ae18151bff189a803094c8b6164
Brayan Jaramillo <bryn07@gmail.com>
2025-02-02 14:04:13 -0500

    Add instructions to app and extract class into its own module.

--- debate-ui/src/app.py
+++ debate-ui/src/app.py
@@ -25,6 +25,7 @@ from debate_for_ai_alignment.pipelines.debate.models import (
 from debate_for_ai_alignment.pipelines.preprocessing.models import (
     UniqueSet,
 )
+from .data_manager import ResultsDataManager
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -36,71 +37,6 @@ NAME_TO_RESULT_CLASS_MAP = {
     "unstructured_debate": UniqueSetUnstructuredDebateResults,
     "structured_debate": UniqueSetStructuredDebateResults,
 }
-
-
-class ResultsDataManager:
-    def __init__(self):
-        self.naive_map = {}
-        self.expert_map = {}
-        self.consultancy_map = {}
-        self.unstructured_debate_map = {}
-        self.structured_debate_map = {}
-        self.unique_set_ids = set()
-        self.unique_set_id_to_debate_style_map: Dict[str, List[str]] = defaultdict(list)
-        self.unique_set_id_to_metadata_map: Dict[str, UniqueSet] = {}
-
-    def get_available_debate_styles(self, unique_set_id):
-        return self.unique_set_id_to_debate_style_map[unique_set_id]
-
-    def get_results(
-        self, unique_set_id, debate_style
-    ) -> Union[
-        UniqueSetNaiveJudgeResults,
-        UniqueSetExpertJudgeResults,
-        UniqueSetConsultancyResults,
-        UniqueSetUnstructuredDebateResults,
-        UniqueSetStructuredDebateResults,
-    ]:
-        if debate_style == "naive":
-            return self.naive_map[unique_set_id]
-        elif debate_style == "expert":
-            return self.expert_map[unique_set_id]
-        elif debate_style == "consultancy":
-            return self.consultancy_map[unique_set_id]
-        elif debate_style == "unstructured_debate":
-            return self.unstructured_debate_map[unique_set_id]
-        elif debate_style == "structured_debate":
-            return self.structured_debate_map[unique_set_id]
-        else:
-            raise ValueError("Unknown debate style")
-
-    def add_unique_set_metadata(self, input_data: UniqueSet):
-        self.unique_set_id_to_metadata_map[input_data.set_unique_id] = input_data
-
-    def add_results(self, results: BaseModel):
-        debate_style = None
-        if isinstance(results, UniqueSetNaiveJudgeResults):
-            self.naive_map[results.results[0].unique_set_id] = results
-            debate_style = "naive"
-        elif isinstance(results, UniqueSetExpertJudgeResults):
-            self.expert_map[results.results[0].unique_set_id] = results
-            debate_style = "expert"
-        elif isinstance(results, UniqueSetConsultancyResults):
-            self.consultancy_map[results.results[0].unique_set_id] = results
-            debate_style = "consultancy"
-        elif isinstance(results, UniqueSetUnstructuredDebateResults):
-            self.unstructured_debate_map[results.results[0].unique_set_id] = results
-            debate_style = "unstructured_debate"
-        elif isinstance(results, UniqueSetStructuredDebateResults):
-            self.structured_debate_map[results.results[0].unique_set_id] = results
-            debate_style = "structured_debate"
-        else:
-            raise ValueError("Unknown results type")
-        self.unique_set_ids.add(results.results[0].unique_set_id)
-        self.unique_set_id_to_debate_style_map[results.results[0].unique_set_id].append(
-            debate_style
-        )
-
 
 def _normalize_log_prob_to_probability(
     log_prob: List[TopLogprob], is_correct_option_first: bool
@@ -117,6 +53,19 @@ def _normalize_log_prob_to_probability(
         np.exp(correct_log_prob) + np.exp(incorrect_log_prob)
     )
 
+APP_INSTRUCTIONS = (
+    "## Instructions\n"
+    "1. Select a unique set ID from the dropdown menu. These ids corresponds to a unique set of questions written by the same author on a single article."
+    "  Different 'unique-sets' may contain questions on the same article.\n"
+    "2. Select a question from the dropdown menu. Unique sets may contain 1-3 questions. This will display the question text, the correct answer, and the best distractor in the `Article Information` section.\n"
+    "3. Select a debate style from the dropdown menu, whether the correct option is listed first as 'A' or second as 'B', and any other options in the dropdown menus that appear. This will display the debate messages and the confidence of the agents in the `Debate` section.\n"
+    "4. Click the `Collapse/Expand Article Content` button to hide or show the full article content. This is the text that is shown to the debaters and consultants, but not the non-expert judges.\n"
+    "5. Click the `Collapse/Expand Debate` button to hide or show the debate messages and confidence scores.\n"
+    "## Confidence Scores\n"
+    "The confidence scores are calculated by taking the log probability of the correct answer and the incorrect answer from the language model, and normalizing them to a probability between 0 and 1.\n"
+    "We sample the log probabilities of the correct and incorrect answers from the language model at the end of each round of debate to get a sense of how these change over the course of the debate.\n"
+)
+
 
 class ResultsViewingApplication:
     def __init__(self, app: dash.Dash, project_path: str):
@@ -128,9 +77,8 @@ class ResultsViewingApplication:
         self._add_callbacks()
 
     def _load_data_kind(self, kind: str, catalog):
-        data = catalog.load(f"partitioned_unique_set_{kind}_results")
-        for load_unique_set_results_func in data.values():
-            raw_result = load_unique_set_results_func()
+        data = catalog.load(f"partitioned_unique_set_{kind}_results@incremental")
+        for raw_result in data.values():
             results = NAME_TO_RESULT_CLASS_MAP[kind](**raw_result)
             self.data_manager.add_results(results)
 
@@ -637,6 +585,9 @@ class ResultsViewingApplication:
         self.app.layout = dbc.Container(
             [
                 dbc.Row([html.H1("Review of Debate Results")]),
+                dbc.Row([
+                    dcc.Markdown(APP_INSTRUCTIONS)
+                ]),
                 dbc.Row(
                     [
                         dbc.Col(
---
+++ debate-ui/src/data_manager.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from typing import Union, Dict, List
+
+from pydantic import BaseModel
+
+from debate_for_ai_alignment.pipelines.debate.models import UniqueSetNaiveJudgeResults, UniqueSetExpertJudgeResults, \
+    UniqueSetConsultancyResults, UniqueSetUnstructuredDebateResults, UniqueSetStructuredDebateResults
+from debate_for_ai_alignment.pipelines.preprocessing.models import UniqueSet
+
+
+class ResultsDataManager:
+    def __init__(self):
+        self.naive_map = {}
+        self.expert_map = {}
+        self.consultancy_map = {}
+        self.unstructured_debate_map = {}
+        self.structured_debate_map = {}
+        self.unique_set_ids = set()
+        self.unique_set_id_to_debate_style_map: Dict[str, List[str]] = defaultdict(list)
+        self.unique_set_id_to_metadata_map: Dict[str, UniqueSet] = {}
+
+    def get_available_debate_styles(self, unique_set_id):
+        return self.unique_set_id_to_debate_style_map[unique_set_id]
+
+    def get_results(
+        self, unique_set_id, debate_style
+    ) -> Union[
+        UniqueSetNaiveJudgeResults,
+        UniqueSetExpertJudgeResults,
+        UniqueSetConsultancyResults,
+        UniqueSetUnstructuredDebateResults,
+        UniqueSetStructuredDebateResults,
+    ]:
+        if debate_style == "naive":
+            return self.naive_map[unique_set_id]
+        elif debate_style == "expert":
+            return self.expert_map[unique_set_id]
+        elif debate_style == "consultancy":
+            return self.consultancy_map[unique_set_id]
+        elif debate_style == "unstructured_debate":
+            return self.unstructured_debate_map[unique_set_id]
+        elif debate_style == "structured_debate":
+            return self.structured_debate_map[unique_set_id]
+        else:
+            raise ValueError("Unknown debate style")
+
+    def add_unique_set_metadata(self, input_data: UniqueSet):
+        self.unique_set_id_to_metadata_map[input_data.set_unique_id] = input_data
+
+    def add_results(self, results: BaseModel):
+        debate_style = None
+        if isinstance(results, UniqueSetNaiveJudgeResults):
+            self.naive_map[results.results[0].unique_set_id] = results
+            debate_style = "naive"
+        elif isinstance(results, UniqueSetExpertJudgeResults):
+            self.expert_map[results.results[0].unique_set_id] = results
+            debate_style = "expert"
+        elif isinstance(results, UniqueSetConsultancyResults):
+            self.consultancy_map[results.results[0].unique_set_id] = results
+            debate_style = "consultancy"
+        elif isinstance(results, UniqueSetUnstructuredDebateResults):
+            self.unstructured_debate_map[results.results[0].unique_set_id] = results
+            debate_style = "unstructured_debate"
+        elif isinstance(results, UniqueSetStructuredDebateResults):
+            self.structured_debate_map[results.results[0].unique_set_id] = results
+            debate_style = "structured_debate"
+        else:
+            raise ValueError("Unknown results type")
+        self.unique_set_ids.add(results.results[0].unique_set_id)
+        self.unique_set_id_to_debate_style_map[results.results[0].unique_set_id].append(
+            debate_style
+        )
